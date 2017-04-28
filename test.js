f52fd01d6fc6fe041e1e279f9fe2b84d93981e0d
Kevin Martensson <kevinmartensson@gmail.com>
2017-04-28 12:18:09 +0200

    Tweak tests

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 import test from 'ava';
 import execa from 'execa';
 
-test('show help screen', async t => {
-	t.regex(await execa.stdout('./cli.js', ['--help']), /Get all gists from a GitHub user/);
-});
-
-test('show version', async t => {
-	t.is(await execa.stdout('./cli.js', ['--version']), require('./package').version);
+test(async t => {
+	t.true((await execa.stdout('./cli.js', ['sindresorhus'])).length > 0);
 });
